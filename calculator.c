d775206b9269cdcbd9225315bc1058c9cebce7e1
Pranjal <pranjalneu@gmail.com>
2024-09-06 10:51:55 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -4,6 +4,8 @@
 int addition(int a, int b);
 int subtraction(int a, int b);
 int multiplication(int a, int b);
+int division(int a, int b);
+
 int main()
 {
 	int num1, num2;
@@ -17,6 +19,7 @@ int main()
 	addition(num1, num2);
 	subtraction(num1, num2);
 	multiplication(num1, num2);
+	division(num1, num2);
 	return 0;
 }
 int addition(int a, int b) {
@@ -32,5 +35,15 @@ int subtraction(int a, int b){
 int multiplication(int a, int b) {
 	float c = a * b;
 	printf("The multiplication of %d & %d is: %f\n", a, b, c);
+	return 0;
+}
+int division(int a, int b) {
+	float c = (float)a / (float)b;
+	if (b == 0) {
+		printf("INFINITY\n");
+	}
+	else 
+	printf("The division of %d & %d is: %f\n", a, b, c);
+	
 	return 0;
 }
