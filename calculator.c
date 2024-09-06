c81537dfbbd72c83b645bc4d6866df0799e19fa0
Pranjal <pranjalneu@gmail.com>
2024-09-06 10:34:45 -0500

    Implemented subtraction feature. Modified the code so that addition and subtraction are called as a function

--- calculator.c
+++ calculator.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
+
+int addition(int a, int b);
+int subtraction(int a, int b);
 int main()
 {
 	int num1, num2;
+	char operation;
 	float result;
-	
+
 	printf("Please enter first number:\n");
 	scanf("%d", &num1);
 	printf("Please enter second number:\n");
 	scanf("%d", &num2);
-	result = num1 + num2;
-	printf("The addition of your given numbers are: %f\n", result);
+	addition(num1, num2);
+	subtraction(num1, num2);
+	return 0;
+}
+int addition(int a, int b) {
+	float c = a + b;
+	printf("The addition of %d & %d is: %f\n", a, b, c);
+	return 0;
+}
+int subtraction(int a, int b){
+	float c = a - b;
+	printf("The subtraction of %d & %d is: %f\n", a, b, c);
 	return 0;
 }
